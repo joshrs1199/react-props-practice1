ce3e0a950a681e90a8870dda820bb8f61674874d
joshrs1199 <46760607+joshrs1199@users.noreply.github.com>
2019-04-22 14:43:34 -0400

    done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
+import Header from "./Components/Header.js"
+import image from "./picture/download.jpeg"
 
 class App extends Component {
   render() {
-    return <header />;
+    return <div>
+            <Header header="a nice 20 minute nap would be nice right now lmao yea" image={image} />
+           </div>
   }
 }
 
